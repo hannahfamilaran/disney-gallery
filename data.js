346dcb7212da689df5a8b0f0b2f6242c586866bb
Hannah Familaran <hannahfamilaran>
2025-11-27 19:34:51 +0800

    Initial commit of Disney Princess Gallery

---
+++ data.js
@@ -0,0 +1,80 @@
+const princesses = [
+  {
+    name: "Snow White",
+    image: "images/SnowWhite.jpg",
+    movie: "Snow White and the Seven Dwarfs (1937)",
+    description: "A kind princess known for her innocence and love for animals."
+  },
+  {
+    name: "Cinderella",
+    image: "images/Cinderella.jpg",
+    movie: "Cinderella (1950)",
+    description: "A kind-hearted girl who overcomes hardship with grace."
+  },
+  {
+    name: "Aurora",
+    image: "images/Aurora.jpg",
+    movie: "Sleeping Beauty (1959)",
+    description: "A beautiful princess cursed to sleep until true love's kiss."
+  },
+  {
+    name: "Ariel",
+    image: "images/Ariel.jpg",
+    movie: "The Little Mermaid (1989)",
+    description: "A curious mermaid princess who dreams of living on land."
+  },
+  {
+    name: "Belle",
+    image: "images/Belle.jpg",
+    movie: "Beauty and the Beast (1991)",
+    description: "A smart and kind girl who loves reading and adventure."
+  },
+  {
+    name: "Jasmine",
+    image: "images/Jasmine.jpg",
+    movie: "Aladdin (1992)",
+    description: "A strong-willed princess who seeks freedom and true love."
+  },
+  {
+    name: "Pocahontas",
+    image: "images/Pocahontas.jpg",
+    movie: "Pocahontas (1995)",
+    description: "A princess who seeks peace and harmony with nature."
+  },
+  {
+    name: "Mulan",
+    image: "images/Mulan.jpg",
+    movie: "Mulan (1998)",
+    description: "A courageous warrior who disguises herself to protect her family."
+  },
+  {
+    name: "Tiana",
+    image: "images/Tiana.jpg",
+    movie: "The Princess and the Frog (2009)",
+    description: "A hardworking princess with big dreams of opening her own restaurant."
+  },
+  {
+    name: "Rapunzel",
+    image: "images/Rapunzel.jpg",
+    movie: "Tangled (2010)",
+    description: "A spirited princess with long magical hair longing for adventure."
+  },
+  {
+    name: "Merida",
+    image: "images/Merida.jpg",
+    movie: "Brave (2012)",
+    description: "A skilled archer and independent princess who challenges tradition."
+  },
+  {
+    name: "Moana",
+    image: "images/Moana.jpg",
+    movie: "Moana (2016)",
+    description: "A brave princess who sails the ocean to save her people."
+  },
+  {
+    name: "Raya",
+    image: "images/Raya.jpg",
+    movie: "Raya and the Last Dragon (2021)",
+    description: "A determined warrior princess who seeks to unite her kingdom."
+  }
+];
